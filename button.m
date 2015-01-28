9badb64f74399a23cafc0032a15c32a05338236d
mx-xiong <yanyupingshen@126.com>
2015-01-28 16:16:10 +0800

    Imported user code snippets

---
+++ button.m
@@ -0,0 +1,17 @@
+// Button
+// 
+//
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 5701ABD9-98B8-4C7C-9208-B2E3ED68A529
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+  UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(<#x#>, <#y#>, <#width#>, <#height#>);
+    [btn setBackgroundImage:[UIImage imageNamed:@"<#NormalBGImage#>"] forState:UIControlStateNormal];
+    [btn setBackgroundImage:[UIImage imageNamed:@"<#HightlightImage#>"] forState:UIControlStateNormal];
+    [btn setTitle:@"<#title#>" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    btn.backgroundColor = [UIColor clearColor];
+    [btn addTarget:self action:@selector(<#btnAction#>) forControlEvents:UIControlEventTouchUpInside];
+    [<#superview#> addSubview:btn];
